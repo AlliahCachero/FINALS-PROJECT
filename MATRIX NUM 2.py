d81b4eea8d02ec508925c49aaa83552982a0c5ba
AlliahCachero <167743978+AlliahCachero@users.noreply.github.com>
2024-06-19 00:26:41 +0800

    Update MATRIX NUM 2.py

--- MATRIX NUM 2.py
+++ MATRIX NUM 2.py
@@ -7,20 +7,16 @@
 
 import numpy as np
 
-# Define the matrix
 matrix = np.array([
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ])
 
-# Sum of rows
 row_sums = np.sum(matrix, axis=1)
 
-# Sum of columns
 column_sums = np.sum(matrix, axis=0)
 
-# Display the results
 print("Matrix:")
 print(matrix)
 print("The sum of rows:", row_sums)
